316a2149799564be9e2b62e0a8ec94a6fecbae29
Egorov Bogdan <egorova0112@gmail.com>
2025-02-26 18:59:20 +0300

    learnt dinamic array creating sintax

--- task01/test06.cpp
+++ task01/test06.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-using namespace std;
-
-void init(int* array, int size) {
-
-
-}
-
-int* test(int* size) {
-
-	*size = 5;
-	int* array = new int[5];// [] <- amount
-	*(array) = 1;
-	*(array + 1) = 11;
-	*(array + 2) = 111;
-	*(array + 3) = 1111;
-	*(array + 4) = 11111;
-
-	return array;
-}
-
-int main() {
-
-	int size;
-	int* array = test(&size);
-
-	for (int i = 0; i < size; i++) {
-
-		cout << array[i] << " ";
-	}
-	cout << endl;
-
-	delete[] array;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//void init(int* array, int size) {
+//
+//
+//}
+//
+//int* test(int* size) {
+//
+//	*size = 5;
+//	int* array = new int[5];// [] <- amount
+//	*(array) = 1;
+//	*(array + 1) = 11;
+//	*(array + 2) = 111;
+//	*(array + 3) = 1111;
+//	*(array + 4) = 11111;
+//
+//	return array;
+//}
+//
+//int main() {
+//
+//	int size;
+//	int* array = test(&size);
+//
+//	for (int i = 0; i < size; i++) {
+//
+//		cout << array[i] << " ";
+//	}
+//	cout << endl;
+//
+//	delete[] array;
+//
+//	return 0;
+//}
