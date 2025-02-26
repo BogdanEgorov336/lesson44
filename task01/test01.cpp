9c5da6670d0f2e69f4d628b9e09b7323a12ccd4c
Egorov Bogdan <egorova0112@gmail.com>
2025-02-26 18:39:56 +0300

    Add project files.

---
+++ task01/test01.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//using namespace std;
+//
+//void swap(int a, int b) {
+//
+//	int t = a;
+//	a = b;
+//	b = t;
+//}
+//
+//int main() {
+//
+//	int x = 10;
+//	int y = 777;
+//
+//	cout << "Before: " << x << ", " << y << endl;
+//	swap(x, y);
+//	cout << "After: " << x << ", " << y << endl;
+//
+//
+//	return 0;
+//}
---
+++ task01/test02.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//using namespace std;
+//
+//// new/delete
+//// ++/--, +, -
+//// *, &
+//
+//void swap(int* a, int* b) {
+//
+//	int t = *a;
+//	*a = *b;
+//	*b = t;
+//}
+//
+//int main() {
+//
+//	int x = 10;
+//	int y = 777;
+//
+//	cout << "Before: " << x << ", " << y << endl;
+//
+//	swap(&x, &y);
+//
+//	cout << "After: " << x << ", " << y << endl;
+//
+//
+//	return 0;
+//}
